9bc8015a46ba9fd930087b80e20282f0fda79511
Damir Fatkulin <internet.zvonok@gmail.com>
2021-10-17 21:38:42 +0600

    [rs-school] task7: Optional. Client application should display alerts for the responses in 401 and 403 HTTP statuses

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://amwwnp8ar3.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://j8rld968fb.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://5ge0sk9kv6.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
