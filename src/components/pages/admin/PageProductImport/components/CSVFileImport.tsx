ee9fbdd8039333ded04fc19671fc10dac42ca3b8
Damir Fatkulin <internet.zvonok@gmail.com>
2021-10-17 22:47:00 +0600

    [rs-school] task7: 7.3: Request from the client application to the /import path of the import-service. Change token

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -31,7 +31,7 @@ export default function CSVFileImport({url, title}: CSVFileImportProps) {
 
   const uploadFile = async (e: any) => {
     const authorizationToken = localStorage.getItem('authorization_token');
-    const headers = { Authorization: authorizationToken };
+    const headers = { Authorization: `Basic ${authorizationToken}` };
     console.log('AuthorizationToken: ', authorizationToken);
       // Get the presigned URL
       const response = await axios({
